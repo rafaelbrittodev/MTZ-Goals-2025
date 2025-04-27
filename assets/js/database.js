7afc9c03846bfc13cb991707c9fae6b0c4114bbc
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2025-04-27 15:47:54 -0300

    v.002-27-04-2025
    Adicionado a página de painel de admistração MTZ com a possibilidade de depositar notas no caixa eletrônico;
    Adicionado JS com localStorage para armazenar as mudanças com depósito de notas bancárias e também um arquivo JS que coleta esses dados e deixa disponíveis em uma variável global.
    Adicionado lógica de carregamento de localStorage ao atualizar todas as páginas.
    Excluídos arquivos JS, CSS e JPEG não utlizados, mas que estavam disponíveis devido ao template utilizado.
    Adicionado pequenos comentários para facilitar o entendimento do localStorage.

---
+++ assets/js/database.js
@@ -0,0 +1,11 @@
+const caixaEletronico = {
+    notas2: 2,
+    notas5: 4,
+    notas10: 8,
+    notas20: 16,
+    notas50: 32,
+    notas100: 64,
+    notas200: 128,
+};
+
+export { caixaEletronico };
---
+++ assets/js/globalVariables.js
@@ -0,0 +1,10 @@
+// assets/js/globalVariables.js
+window.caixaEletronico = {
+    notas2: 0,
+    notas5: 0,
+    notas10: 0,
+    notas20: 0,
+    notas50: 0,
+    notas100: 0,
+    notas200: 0,
+  };
